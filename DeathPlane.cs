c3b413c3b7dfea08b69b0ac385b333c74cd3a60b
Thijs <71495073+Thijs-Ruiter@users.noreply.github.com>
2022-01-24 11:09:41 +0100

    Update DeathPlane.cs

--- DeathPlane.cs
+++ DeathPlane.cs
@@ -1,19 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class DeathPlane : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public GameObject player;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
